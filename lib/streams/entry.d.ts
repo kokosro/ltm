d363fd2eb3d2d4b61e1220c3871b825093cd4741
kokosro <robert@kokos.ro>
2022-10-15 12:18:56 +0300

    removed build folder

--- lib/streams/entry.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="node" />
-import { Readable } from 'stream';
-export declare class EntryStream extends Readable {
-    constructor();
-    _read(): void;
-    received(value: any): void;
-}
